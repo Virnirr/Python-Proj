4ad0a09e895ee7cfc2412cce3820985e235ebac7
Virnir <zhihe122@gmail.com>
2021-09-13 20:24:42 -0700

    some changes

--- image.py
+++ image.py
@@ -6,11 +6,11 @@ import os
 path = sys.argv[1]
 directory = sys.argv[2]
 
-# check if new/ exists, if not create it
+# check if directory new/ exists, if not create it
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-# loop through Pokedex,
+# loop through images in a directory/path
 for filename in os.listdir(path):
     img = Image.open(f'{path}{filename}')
     clean_name = os.path.splitext(filename)[0]
