005f56a05cdcf294f8aca708cc4db99f62c9c4db
Zhi He <86027312+Virnirr@users.noreply.github.com>
2021-09-22 16:49:08 -0700

    Update num_guess_game.py

--- Games/num_guess_game.py
+++ Games/num_guess_game.py
@@ -3,7 +3,7 @@ import sys
 
 if len(sys.argv) != 3:
     sys.exit("Invalid Commands. There should only have 3 commands.")
-
+# This will check for error
 try:
     if int(sys.argv[1]) > int(sys.argv[2]):
         sys.exit("First integer must be greater than the second integer")
